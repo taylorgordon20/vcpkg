07e258735060a7f6dbc52943692e549fbff8c2c3
Robert Schumacher <roschuma@microsoft.com>
2018-09-28 02:18:24 -0700

    wip

--- ports/libvorbis/portfile.cmake
+++ ports/libvorbis/portfile.cmake
@@ -1,39 +1,25 @@
-# Common Ambient Variables:
-#   VCPKG_ROOT_DIR = <C:\path\to\current\vcpkg>
-#   TARGET_TRIPLET is the current triplet (x86-windows, etc)
-#   PORT is the current port name (zlib, etc)
-#   CURRENT_BUILDTREES_DIR = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR  = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#
-
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/vorbis-112d3bd0aaacad51305e1464d4b381dabad0e88b)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/xiph/vorbis/archive/112d3bd0aaacad51305e1464d4b381dabad0e88b.zip"
-    FILENAME "vorbis-112d3bd0aaacad51305e1464d4b381dabad0e88b.zip"
-    SHA512 94e773a34f3e8d1c8ed0422f0eab345b35f76a96760141af83d69d007ebf076fca2d083a77d36bfa4ea10dfefa03a8fa35201aced963655ab8a524aaa7580b11
-)
 
-vcpkg_extract_source_archive(${ARCHIVE})
-vcpkg_apply_patches(SOURCE_PATH ${SOURCE_PATH}
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO xiph/vorbis
+    REF 112d3bd0aaacad51305e1464d4b381dabad0e88b
+    SHA512 df20e072a5e024ca2b8fc0e2890bb8968c0c948a833149a6026d2eaf6ab57b88b6d00d0bfb3b8bfcf879c7875e7cfacb8c6bf454bfc083b41d76132c567ff7ae
+    HEAD_REF master
     PATCHES
-        ${CMAKE_CURRENT_LIST_DIR}/0001-Dont-export-vorbisenc-functions.patch
-        ${CMAKE_CURRENT_LIST_DIR}/0002-Allow-deprecated-functions.patch
+        0001-Dont-export-vorbisenc-functions.patch
+        0002-Allow-deprecated-functions.patch
+        targets.patch
 )
-
-file(TO_NATIVE_PATH "${VCPKG_ROOT_DIR}/installed/${TARGET_TRIPLET}/include" OGG_INCLUDE)
-file(TO_NATIVE_PATH "${VCPKG_ROOT_DIR}/installed/${TARGET_TRIPLET}/lib/ogg.lib" OGG_LIB_REL)
-file(TO_NATIVE_PATH "${VCPKG_ROOT_DIR}/installed/${TARGET_TRIPLET}/debug/lib/ogg.lib" OGG_LIB_DBG)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
-    OPTIONS -DOGG_INCLUDE_DIRS=${OGG_INCLUDE}
-    OPTIONS_RELEASE -DOGG_LIBRARIES=${OGG_LIB_REL}
-    OPTIONS_DEBUG -DOGG_LIBRARIES=${OGG_LIB_DBG}
 )
 
 vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-vorbis TARGET_PATH share/unofficial-vorbis)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
@@ -41,4 +27,8 @@ file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/libvorbis)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/libvorbis/COPYING ${CURRENT_PACKAGES_DIR}/share/libvorbis/copyright)
 
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/unofficial-vorbis-config.cmake DESTINATION ${CURRENT_PACKAGES_DIR}/share/unofficial-vorbis)
+
 vcpkg_copy_pdbs()
+
+vcpkg_test_cmake(PACKAGE_NAME unofficial-vorbis)
